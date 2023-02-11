478dd8d76501b46898f4d189727d6f949ec05c9f
vincent <gvincentvijay@temenos.com>
2023-02-10 23:16:59 -0500

    SeleniumRedo

---
+++ src/main/java/week1/day1/HelloWorld.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+		
+		//primitive data types:
+		
+		int number = 123333;
+		byte smallno = 12;
+		short neg = -3422;
+		float floatTest = 122.11222222f;
+		long testLong = 122222222222222222l;
+	}
+}
---
+++ src/main/java/week1/day2/PrimeCheck.java
@@ -0,0 +1,28 @@
+package week1.day2;
+
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class PrimeCheck {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sn = new Scanner(System.in);
+		System.out.println("Enter the number: ");
+		int input = sn.nextInt();
+		for (int i = 2	; i < input; i++) {
+	
+			if(input%i==0) {
+				System.out.println("Number is not  prime: "+i);
+			}
+//	3
+			else {
+				System.out.println("Number is not prime");
+			}
+			}
+			
+		}
+
+	}
+
+
